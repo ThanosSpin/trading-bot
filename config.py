a00dc5555a0ae980fa88db11f264cb7e9b866e01
Thanos Spinoulas <thanosspinoulas@192.168.2.22>
2025-05-14 23:25:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,13 @@ load_dotenv()  # Load from .env file
 
 API_KEY = os.getenv("ALPACA_API_KEY")
 API_SECRET = os.getenv("ALPACA_SECRET_KEY")
+API_MARKET_KEY = os.getenv("ALPACA_MARKET_API_KEY")
+API_MARKET_SECRET = os.getenv("ALPACA_MARKET_SECRET_KEY")
 BASE_URL = os.getenv("ALPACA_BASE_URL")
+MARKET_BASE_URL = os.getenv("ALPACA_MARKET_BASE_URL") # For live trading
+EMAIL_SENDER = os.getenv("EMAIL")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")  # Use App Password
+EMAIL_RECEIVER = os.getenv("EMAIL_RECEIVER")
 
 SYMBOL = 'NVDA'
 INITIAL_CAPITAL = 0
