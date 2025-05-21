5da09587b2b862e6ba4efba08422888db444eac7
Thanos Spinoulas <thanosspinoulas@192.168.2.22>
2025-05-21 23:03:09 +0300

    updated dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,16 +3,22 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import pytz
 from config import PORTFOLIO_PATH
 from portfolio import get_live_portfolio  # ✅ Use live data
+from config import TIMEZONE
 
 TRADE_LOG_PATH = "data/trade_log.csv"
 PLOT_PATH = "data/portfolio_performance.png"
 
 def load_trade_log():
     if os.path.exists(TRADE_LOG_PATH):
-        return pd.read_csv(TRADE_LOG_PATH, parse_dates=["timestamp"])
-    return pd.DataFrame(columns=["timestamp", "action", "price"])
+        df = pd.read_csv(TRADE_LOG_PATH, parse_dates=["timestamp"])
+        local_tz = pytz.timezone(TIMEZONE)
+        df["timestamp"] = df["timestamp"].dt.tz_localize("UTC").dt.tz_convert(local_tz)
+        df["timestamp"] = df["timestamp"].dt.strftime("%Y-%m-%d %H:%M:%S")
+        return df
+    return pd.DataFrame(columns=["timestamp", "action", "price", "value"])
 
 # Streamlit app
 st.set_page_config(page_title="Trading Bot Dashboard", layout="wide")
